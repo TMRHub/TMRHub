bb7cbb91705f9377d6ea5f2982ed629606729c4f
TMRHub <146746901+TMRHub@users.noreply.github.com>
2023-10-02 17:40:16 +0300

    Add files via upload

---
+++ tmr-10system/readme.lua
@@ -0,0 +1,46 @@
+TMRHUB-Development
+erellocha
+
+to make it work just use our pma-voice & qb-radio
+
+and dont forget to change the qb-core exports to yours!
+
+
+Enjoy TMRHUB erellocha <3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ps fuck partizan fat cunt
